916972b6cb3808da13072d835912750ca1763733
mozessara <56548464+mozessara@users.noreply.github.com>
2024-11-16 20:36:57 +0100

    Update transfer.sh

--- project-template/transfer.sh
+++ project-template/transfer.sh
@@ -3,4 +3,4 @@
 cd build
 make
 cd ..
-cmd.exe /c "//wsl.localhost/Debian$(find "$(pwd)" -maxdepth 1 -type f -name "*.bat" | head -n 1)" | tr / "\\"
+cmd.exe /c "//wsl.localhost/Ubuntu$(find "$(pwd)" -maxdepth 1 -type f -name "*.bat" | head -n 1)" | tr / "\\"
