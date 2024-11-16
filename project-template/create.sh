32a416569b7339c6de0bc290f3109237a6020c3b
mozessara <56548464+mozessara@users.noreply.github.com>
2024-11-16 20:36:42 +0100

    Update create.sh

--- project-template/create.sh
+++ project-template/create.sh
@@ -5,4 +5,4 @@ cd build
 cmake ..
 make
 cd ..
-cmd.exe /c "//wsl.localhost/Debian$(find "$(pwd)" -maxdepth 1 -type f -name "*.bat" | head -n 1)" | tr / "\\"
+cmd.exe /c "//wsl.localhost/Ubuntu$(find "$(pwd)" -maxdepth 1 -type f -name "*.bat" | head -n 1)" | tr / "\\"
